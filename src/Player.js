fd4ed8aac16006d0686c6e307cfb6fde37130e8e
Angel <kyliann.faggian@gmail.com>
2022-04-04 15:48:56 +0200

    sauts

--- src/Player.js
+++ src/Player.js
@@ -5,9 +5,10 @@ class Player {
         this.scene=scene
         this.cameras=scene
         this.player = this.scene.physics.add.sprite(50, 300, 'player');
-        this.player.setBounce(0.1);
+        this.player.setBounce(0);
         this.player.setCollideWorldBounds(false);
         this.scene.physics.add.collider(this.player, this.scene.platforms);
+        this.scene.physics.add.overlap(this.player, this.scene.mur,this.onmur,null,this);
 
         this.scene.anims.create({
             key: 'walk',
@@ -33,6 +34,65 @@ class Player {
 
         });
     }
+    onmur(){
+        this.player.body.blocked.down=true
+        console.log('sur le mur')
+    }
+
+    //LE DASH
+    create(){
+        this.speed={
+            speedDash:1,
+        }
+
+        this.dash = this.tweens.add({
+            targets: this.speed,
+            speedDash: 1,
+            // alpha: { start: 0, to: 1 },
+            // alpha: 1,
+            // alpha: '+=1',
+            ease: "Circ.easeInOut", // 'Cubic', 'Elastic', 'Bounce', 'Back'
+            duration: 300,
+            //repeat: -1, // -1: infinity
+            //yoyo: true
+        });
+
+//CREATION DES ACTIONS DU PERSONNAGE
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+
+        //this.player.setBounce(0.2);// REBONDISSEMENT DU PERSONNAGE LORSQU'IL SAUTE
+        this.player.setCollideWorldBounds(true);//COLLISION AVEC TOUS LES OBJETS DU JEU
+//CREATION DES ANIMATIONS DU PERSONNAGE GRACE AU SPRITESHEET
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),//CE SONT LES IMAGES 0/1/2/3 QUI SONT JOUEES
+            frameRate: 10,//NOMBRE D'IMAGES JOUEES
+            repeat: -1//REPETITION INFINIE
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],//C'EST L'IMAGE 4 QUI EST JOUEE
+            frameRate: 20//NOMBRE D'IMAGES JOUEES
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),//CE SONT LES IMAGES 5/6/7/8 QUI SONT JOUEES
+            frameRate: 10,//NOMBRE D'IMAGES JOUEES
+            repeat: -1//REPETITION INFINIE
+        });
+
+        this.anims.create({
+            key: 'tombe',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),//CE SONT LES IMAGES 5/6/7/8 QUI SONT JOUEES
+            frameRate: 10,//NOMBRE D'IMAGES JOUEES
+            repeat: 1//REPETITION INFINIE
+        });
+
+
+    }
+
 
     jump(){
         this.player.setVelocityY(-420);
--- src/scene.js
+++ src/scene.js
@@ -16,41 +16,76 @@ class scene extends Phaser.Scene {
 
 
 
-
-
         const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
         backgroundImage.setScale(1, 0.8);
         const map = this.make.tilemap({key: 'map'});
 
         const tileset = map.addTilesetImage('Alpha_test1', 'tiles');
         this.platforms = map.createStaticLayer('Sol', tileset);
+        this.mur = map.createStaticLayer('mur', tileset);
 
         this.platforms.setCollisionByExclusion(-1, true);
+        this.mur.setCollisionByExclusion(-1, true);
         this.cursors = this.input.keyboard.createCursorKeys();
 
 
         this.player = new Player(this)
 
         this.cameras.main.startFollow(this.player.player,false);
+        this.initKeyboard()
+
+    }
+    initKeyboard() {
+        let me = this;
+        this.input.keyboard.on('keydown', function (kevent) {
+            switch (kevent.keyCode) {
+                case Phaser.Input.Keyboard.KeyCodes.SHIFT:
+                    me.shiftDown=true;
+                    me.dash=true;
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.SPACE:
+                    me.spaceDown=true;
+
+                    if (me.player.player.body.onFloor())
+                    {
+                        me.player.jump();//LE JOUR VA A UNE VITESSE DE 330 VERS LE HAUT
+                    }
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.D:
+                    me.rightDown=true;
+                    me.player.moveRight();//LE PERSONNAGE VA A UNE VITESSE DE A UNE VITESSE DE 160 A DROITE
+                    //ET ON LUI DEMANDE DE LANCER L'ANIMATION RIGHT QU'ON A CREE DANS LA FONCTION CREATE
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.Q:
+                    me.leftDown=true;
+                    me.player.moveLeft();//LE PERSONNAGE VA A UNE VITESSE DE A UNE VITESSE DE 160 A GAUCHE
+                    //ET ON LUI DEMANDE DE LANCER L'ANIMATION LEFT QU'ON A CREE DANS LA FONCTION CREATE
+                    break;
+            }
+        });
+        this.input.keyboard.on('keyup', function (kevent) {
+            switch (kevent.keyCode) {
+                case Phaser.Input.Keyboard.KeyCodes.SHIFT:
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.SPACE:
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.D:
+                    me.player.stop()
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.Q:
+                    me.player.stop()
+                    break;
+
+            }
+        });
     }
 
 
     update() {
+        console.log(this.player.player.body.onWall())
 
-        switch (true) {
-            case (this.cursors.space.isDown || this.cursors.up.isDown) && this.player.player.body.onFloor():
-                this.player.jump()
-                console.log("oui")
-                break;
-            case this.cursors.left.isDown:
-                this.player.moveLeft()
-                break;
-            case this.cursors.right.isDown:
-                this.player.moveRight();
-                break;
-            default:
-                this.player.stop();
-        }
+
+
 
 
 
