38c087e3f60a123d829efc8c34ddd4890abb9f31
NanoLoriginal <kyliann.faggian@gmail.com>
2022-06-03 16:35:11 +0200

    phaser = flaque de pisse d'el pueblo

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 600 },
-            debug: true,
+            debug: false,
             fps: 140,
         },
     },
